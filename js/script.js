8195ea093809f559b2a8d8b1b5af3eed352e928c
Kirill <kirillkolesnichenko21@gmail.com>
2021-10-24 15:47:00 +0300

    new section + scrollify

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 jQuery(document).ready(function ($) {
   $.scrollify({
     section: ".scrollify",
-    interstitialSection: "",
-    easing: "easeOutExpo",
     scrollSpeed: 100,
-    offset: 0,
-    scrollbars: true,
     setHeights: false,
-    overflowScroll: true,
     updateHash: false,
     touchScroll: true,
   });
 });
-// ----------------------------------------------------
